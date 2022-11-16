43303e6d11c21e90e98c9b0aa901f6d5e0ae5517
Arvindh1509 <72424948+Arvindh1509@users.noreply.github.com>
2022-11-16 13:58:18 +0530

    Initial commit

---
+++ client/src/components/InfoBar/InfoBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+import closeIcon from '../../icons/closeIcon.png';
+//  import { Avatar, IconButton } from '@mui/material'
+// import { useState , useEffect} from 'react' 
+
+
+import './InfoBar.css';
+
+ function InfoBar  ({ room })  {
+
+
+
+return(
+  <div className="infoBar">
+    <div className="leftInnerContainer">
+    
+      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+     
+      <h3 style={{color:"white"}}>{room}</h3>
+    </div>
+    <div className="rightInnerContainer">
+      <a href="/"><img src={closeIcon} alt="close icon" /></a>
+    </div>
+  </div>
+)
+};
+
+export default InfoBar;
+
